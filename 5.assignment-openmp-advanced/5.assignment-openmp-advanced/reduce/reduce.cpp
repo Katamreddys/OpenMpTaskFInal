6697854b82d381a67ab82822f975b366d9f6a2d7
Katamreddys <47572608+Katamreddys@users.noreply.github.com>
2019-03-26 11:14:54 -0400

    Update reduce.cpp

--- 5.assignment-openmp-advanced/5.assignment-openmp-advanced/reduce/reduce.cpp
+++ 5.assignment-openmp-advanced/5.assignment-openmp-advanced/reduce/reduce.cpp
@@ -19,14 +19,38 @@ extern "C" {
 }
 #endif
 
-//Compute sum for each thread using the follwoing function
-int compute_thread_sum(int *arr, int loop_start, int loop_end){
-  int partial_sum;
-  for(int j=loop_start;j<loop_end;j++){
-    partial_sum+=arr[j];
+int total_arr_sum;
+
+void reduce_sum_calculation(int *arr, int graularity, int n){
+  #pragma omp parallel
+  {
+    int arr_thread_sum;
+    #pragma omp single 
+    {
+      for(int i=0; i<n; i+=graularity){
+        int loop_start = i;
+        int loop_end = loop_start+graularity;
+        if(loop_end>n){
+          loop_end = n;
+        }
+        #pragma omp task
+        {
+          int arr_thread_sum;
+          for(int j=loop_start;j<loop_end;j++){
+            arr_thread_sum+=arr[j];
+          }
+        }
+        #pragma omp critical
+        {
+          total_arr_sum+=arr_thread_sum;
+        }
+      }
+    }
   }
-  return partial_sum;
 }
+
+
+
 
 
 int main (int argc, char* argv[]) {
@@ -49,46 +73,23 @@ int main (int argc, char* argv[]) {
 
 
   int n = atoi(argv[1]);
+  
+  
+  
+  total_arr_sum = 0;
+  int * arr = new int [n];
   int nbthreads= atoi(argv[2]);
   int graularity = n/nbthreads;
-  int * arr = new int [n];
-  int i;
-  
-  int reduce_sum = 0;
   omp_set_num_threads(nbthreads);
   generateReduceData (arr, atoi(argv[1]));
 
   //insert reduction code here
-  auto clock_start = std::chrono::system_clock::now();//Start of clock just bnefore merge begins. 
-  #pragma omp parallel
-  {
-    int partial_sum;//Initialting partial_sum for each thread
-    #pragma omp single 
-    {
-      //Split the array with size of granularity each and implement reduce for each array
-      for(int i=0; i<n; i+=graularity){
-        int loop_start = i;
-        int loop_end = loop_start+graularity;
-        if(loop_end>n){
-          loop_end = n;
-        }
-        //implement task on each partial sum and calculate for each array
-        #pragma omp task
-        {
-          partial_sum = compute_thread_sum(arr, loop_start, loop_end);
-        }
-        //Add all partial sum and get hte reduce sum 
-        #pragma omp critical
-        {
-          reduce_sum+=partial_sum;
-        }
-      }
-    }
-  }
-  auto clock_end = std::chrono::system_clock::now();//End of clock once merge ends.
-  std::chrono::duration<double> total_time = clock_end-clock_start;//Total time for execution.
+  auto timeStart = std::chrono::system_clock::now();
+  reduce_sum_calculation(arr,graularity,n);
+  auto timeEnd = std::chrono::system_clock::now();
+  std::chrono::duration<double> total_time = timeEnd-timeStart;
 
-  std::cout<<reduce_sum<<std::endl;
+  std::cout<<total_arr_sum<<std::endl;
 
   std::cerr<<total_time.count()<<std::endl;
     
